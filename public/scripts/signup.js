821c8911eaf107b2b2f85c5c0cb0ee204a429f7a
doppi08 <igy052424@student.skelleftea.se>
2026-09-17 10:06:18 +0200

    Test

---
+++ public/scripts/signup.js
@@ -0,0 +1,3 @@
+const usernameField = document.getElementById("usernameField")
+const passwordField = document.getElementById("passwordField")
+const loginButton = document.getElementById("loginButton")
